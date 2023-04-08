abd67aab06a4e1fdc3845b348ce03fe145d2a948
sonalitchaudhari <sonalitchaudhari@gmail.com>
2023-04-08 16:32:29 +0530

    letest commit

--- src/test/java/FirstTestCase1/SonaliC.java
+++ src/test/java/FirstTestCase1/SonaliC.java
@@ -1,12 +1,9 @@
 package FirstTestCase1;
 
 public class SonaliC {
-
-		  public void m1() {
-          System.out.println("sonali");
-          System.out.println("hello");
-          System.out.println("Hiii");
-          System.out.println("check");
-	}
+public static void main(String[] args) {
+	System.out.println("sonali");
+		 
+}
 
 }
