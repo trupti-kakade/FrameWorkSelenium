2168a31ae4cc16290014910e1a23645b50ca196c
sonalitchaudhari <sonalitchaudhari@gmail.com>
2023-04-08 16:44:45 +0530

    my letest 2 commit

--- src/test/java/FirstTestCase1/SonaliC.java
+++ src/test/java/FirstTestCase1/SonaliC.java
@@ -1,9 +1,13 @@
 package FirstTestCase1;
 
 public class SonaliC {
-public static void main(String[] args) {
-	System.out.println("sonali");
-		 
-}
+	public static void main(String[] args) {
+		System.out.println("sonali");
+		System.out.println("testing");
+		System.out.println("shastra");
+		System.out.println("class");
+
+
+	}
 
 }
